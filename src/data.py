bb8d7aa93ebe057b0ae8d091eccf12f8bc9de6cc
“Jake <jfawkes@spotify.com>
2024-08-16 10:06:27 +0100

    first commit

---
+++ src/data.py
@@ -0,0 +1,128 @@
+import torch
+from collections import namedtuple
+import gpytorch
+from src.GP_bounds import ExactGPModel
+
+kallus_func = lambda X,U,T,eps: 1+T+X+2*T*X+0.5*X**2+0.75*T*X**2+U+0.5*eps
+cov_matricies = []
+data = namedtuple("data","X T Y")
+
+outcome_funcs = namedtuple("Outcome_funcs","cfounded_func uncfounded_func")
+def get_pseudo_outcome_data(exp_data,T_prop=0.5):
+    pseudo_outcome = (exp_data.T - T_prop)/((T_prop)*(1-T_prop))*exp_data.Y  
+    pseudo_data = data(X=exp_data.X, T=exp_data.T,Y=pseudo_outcome)
+    return pseudo_data
+
+def get_conditioned_data(initial_data,T_val):
+    truth_vec = initial_data.T == T_val
+    return data(X=initial_data.X[truth_vec],Y=initial_data.Y[truth_vec],T=initial_data.T[truth_vec])
+
+def adjust_data(initial_data,adjustment_func):
+    adjust_Y = initial_data.Y - adjustment_func(initial_data.X,initial_data.T)
+    return data(X=initial_data.X,Y=adjust_Y,T=initial_data.T)
+
+for i in range(2):
+    t = i-0.5
+    cov_matricies.append(torch.tensor([[1,t],[t,1]]))
+
+def get_exp_sample_kallus(n_samples,X_range=(-1,1),T_prop=0.5,outcome_func = kallus_func):
+    X = (X_range[1] - X_range[0]) * torch.rand(n_samples) + X_range[0]
+    T = (torch.rand(n_samples) > T_prop).type(torch.float)
+    U = torch.randn(n_samples)
+    eps = torch.randn(n_samples)
+    Y = outcome_func(X,U,T,eps)
+    return data(X,T,Y)
+
+def get_obs_sample_kallus(n_samples,X_range=(-3,3),T_prop=0.5,cov_matricies=cov_matricies,outcome_func = kallus_func):
+    T = (torch.rand(n_samples) > T_prop).type(torch.float)
+    X,U = torch.zeros(n_samples),torch.zeros(n_samples)
+    for i in range(2):
+        t_dist = torch.distributions.multivariate_normal.MultivariateNormal(loc= torch.zeros(len(cov_matricies[i])) ,covariance_matrix= cov_matricies[i])
+        sample = t_dist.sample((sum(T==i),))
+        X[T==i],U[T==i] = sample[:,0],sample[:,1]
+    eps = torch.randn(n_samples)
+    Y = outcome_func(X,U,T,eps)
+    return data(X,T,Y)
+
+def get_gp_samples(X_eval,hyperparam_dict,train_data=None,likelihood=None):
+
+    if likelihood ==None:
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+    if train_data == None:
+        model = ExactGPModel(None, None, likelihood, dimx = hyperparam_dict["dimx"], lb=hyperparam_dict["lb"], ub=hyperparam_dict["ub"])
+    else:
+       model =  ExactGPModel(train_data.X, train_data.Y, likelihood, dimx = hyperparam_dict["dimx"], lb=hyperparam_dict["lb"], ub=hyperparam_dict["ub"])
+       model.train()
+
+    model.eval()
+    return model(X_eval).sample()
+
+
+dimx = 1
+ub = 1.1*torch.ones(3)
+ub[0] = 1.1
+ub[-1] = 1.1
+lb = 0.9*torch.ones(3)
+lb[-1] = 0.9
+loglikelihood0 = [None]
+training_iterations = 2000
+num_samples = 20
+warmup_steps = 20
+sqrbeta0 = 1.44
+
+hyperparam_dict = {
+    "dimx":dimx,
+    "ub":ub,
+    "lb":lb,
+    "loglikelihood0":loglikelihood0,
+    "training_iterations":training_iterations,
+    "num_samples":num_samples,
+    "warmup_steps":warmup_steps,
+    "sqrbeta0":sqrbeta0
+
+}
+
+def GP_func_1d(X_range,hyperparam_dict= hyperparam_dict
+               ,N_eval_points=5000,train_data=None,likelihood=None,scale=1):
+    X_eval = torch.linspace(X_range[0],X_range[1],steps=N_eval_points)
+    Y_eval = get_gp_samples(X_eval,hyperparam_dict=hyperparam_dict,train_data=train_data,likelihood=likelihood)
+    def GP(X):
+        a = ((N_eval_points-1)*(X-X_range[0])/(X_range[1]-X_range[0])).int()
+        return scale*Y_eval[a]
+    return GP    
+
+def get_train_data_GP_1d(generating_outcome_funcs,
+                         n_samples_exp = 200,n_samples_obs = 1000, 
+                         exp_range = (-1,1),obs_range = (-3,3),
+                         T_prop = 0.5,train_data = None):
+    cfd_GPs = [0,0]
+    ucfd_GPs = [0,0]
+
+    for i in range(2):
+        cfd_GPs[i] = GP_func_1d(obs_range,hyperparam_dict)
+
+        if train_data == None:
+            ucfd_GPs[i] = GP_func_1d(obs_range,hyperparam_dict,train_data=None,scale=1)
+        else:
+            ucfd_GPs[i] = GP_func_1d(obs_range,hyperparam_dict,train_data=get_conditioned_data(train_data,i),scale=1)
+        
+        
+
+    # cfded_GP_func = lambda X,T: (1-T)*cfd_GPs[0](X) + (T)*cfd_GPs[1](X)
+    cfded_GP_func = generating_outcome_funcs.cfounded_func
+    ucfded_GP_func = lambda X,T: cfded_GP_func(X,T) + (1-T)*ucfd_GPs[0](X) + (T)*ucfd_GPs[1](X)
+    outcome_funcs_GP = outcome_funcs(cfounded_func=cfded_GP_func,uncfounded_func=ucfded_GP_func)
+    X_range_obs = obs_range
+    X_obs = (X_range_obs[1] - X_range_obs[0]) * torch.rand(n_samples_obs) + X_range_obs[0]
+    T_obs = (torch.rand(n_samples_obs) > T_prop).type(torch.float)
+    Y_obs = cfded_GP_func(X_obs,T_obs) + torch.randn(n_samples_obs)
+    obs_data_GP = data(X=X_obs,Y=Y_obs,T=T_obs)
+
+    X_range_exp = exp_range
+    X_exp = (X_range_exp[1] - X_range_exp[0]) * torch.rand(n_samples_exp) + X_range_exp[0]
+    T_exp = (torch.rand(n_samples_exp) > T_prop).type(torch.float)
+    Y_exp = ucfded_GP_func(X_exp,T_exp) + torch.randn(n_samples_exp)
+    exp_data_GP = data(X=X_exp,Y=Y_exp,T=T_exp)
+
+    return exp_data_GP,obs_data_GP,outcome_funcs_GP
---
+++ src/plotting.py
@@ -0,0 +1,120 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import gpytorch
+from gpytorch.priors import UniformPrior
+from matplotlib import pyplot as plt
+from matplotlib import rc
+import seaborn as sns
+from collections import namedtuple
+from src.data import *
+
+def plot1D(X_data,y_data,X_test,y_pred, beta, sig_pred, beta_safe, sig_safepr, y_predfb, sig_prfb, f_true):
+    y_pred = y_pred.detach()
+    y_data = y_data.detach()
+
+    # Plot the function, the prediction and the 95% confidence interval
+    rc('font', size=50)
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+    rc('text', usetex=True)
+    
+    fig = plt.figure(num=None, figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot()
+    ax.set_axisbelow(True)
+    
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    
+    
+    plt.plot(X_data, y_data, 'kx', markersize=18, label='Observations',mew=5.0)
+    plt.plot(X_test, f_true, 'k-', label=r'$f(x)$',linewidth=4.0)
+    plt.plot(X_test, y_pred, 'r-.', label=r'$\mu_{\vartheta_0}$(x)',linewidth=4.0)
+    plt.plot(X_test, y_predfb, 'g-.', label=r'$\mu_{{FB}}$(x)',linewidth=4.0)
+    plt.fill(torch.cat([X_test, torch.flip(X_test,[0])]),
+             torch.cat([y_pred - beta_safe * sig_safepr,
+                            torch.flip(y_pred + beta_safe * sig_safepr,[0])]).detach(),
+             alpha=.2, fc='b', ec='None', label= 'Our approach') # r'$\pm \bar{\beta}^{\frac{1}{2}} {\sigma}_{\vartheta^\prime}(x)$')
+    plt.fill(torch.cat([X_test, torch.flip(X_test,[0])]),
+             torch.cat([y_pred - beta * sig_pred,
+                            torch.flip(y_pred + beta * sig_pred,[0])]).detach(),
+             alpha=.2, fc='r', ec='None', label= 'Vanilla GP') # r'$\pm \beta^{\frac{1}{2}} \sigma_{\vartheta_0}(x)$')
+    plt.fill(torch.cat([X_test, torch.flip(X_test, [0])]),
+             torch.cat([y_predfb - beta * sig_prfb,
+                        torch.flip(y_predfb + beta * sig_prfb, [0])]).detach(),
+             alpha=.2, fc='g', ec='None', label='Full Bayes') #r'$\pm \beta^{\frac{1}{2}} \sigma_{{FB}}(x)$')
+    plt.xlabel('$x$')
+    if X_data.shape[0] < 3:
+        plt.ylabel('$f(x)$')
+    ylim_UB = 9.5 # 2**(torch.ceil(torch.log2(torch.max(y_pred+ beta_safe * sig_safepr)))+1)
+    ylim_LB = -3.5 #-2**(torch.ceil(torch.log2(torch.abs(torch.min(y_pred- beta_safe * sig_safepr)))))
+    plt.ylim(ylim_LB, ylim_UB)
+    plt.xlim(X_test.min(),X_test.max())
+    plt.tick_params(
+        axis='both',          # changes apply to both axes
+        which='both',      # both major and minor ticks are affected
+        bottom=False,      # ticks along the bottom edge are off
+        left=False,
+        top=False,         # ticks along the top edge are off
+        labelleft=False,
+        labelbottom=False) # labels along the bottom edge are off
+    plt.tight_layout()
+
+    # if X_data.shape[0] > 8:
+    plt.legend(loc='upper left', ncol=3, prop={'size': 26})
+    handles, labels = ax.get_legend_handles_labels()
+    legend = ax.legend(handles,labels,loc='upper left', ncol=3, prop={'size': 26},frameon=False)
+    ## save legend separately
+    # leg = legend.figure
+    # leg.canvas.draw()
+    # bbox = legend.get_window_extent().transformed(leg.dpi_scale_trans.inverted())
+    # leg.savefig('legend.pdf', format='pdf', bbox_inches=bbox)
+
+    # plt.savefig('safegp_N' + X_data.shape[0].__str__() + '.pdf', format='pdf')
+    
+    plt.show()
+
+
+
+def obs_exp_pseudo_plot(obs_data,exp_data,outcome_funcs,T_prop=0.5):
+
+    pseudo_data = get_pseudo_outcome_data(exp_data,T_prop=T_prop)
+
+    fig, axs = plt.subplots(ncols=3,sharex=True,figsize=(16,4),sharey=True)
+
+    sns.scatterplot(x=obs_data.X,y=obs_data.Y,hue=obs_data.T,ax=axs[0])
+    sns.scatterplot(x=exp_data.X,y=exp_data.Y,hue=exp_data.T,ax=axs[1])
+    sns.scatterplot(x=pseudo_data.X,y=pseudo_data.Y,hue=pseudo_data.T,ax=axs[2])
+
+    axs[0].set_title("Observational Data")
+    axs[1].set_title("Experimental Data")
+    axs[2].set_title("Pseudo Outcomes")
+    col = ["blue","orange"]
+    # 
+
+    for i in range(3):
+        axs[i].legend(labels=["T=0","T=1"])
+        leg = axs[i].get_legend()
+        for j in range(2):
+            leg.legendHandles[j].set_color(col[j])
+        axs[i].set_xlabel("X")
+            
+    axs[0].set_ylabel("Y")
+
+    for i in range(2):
+        X_plot = torch.linspace(-3, 3, 1000)
+        Y_plot = outcome_funcs.cfounded_func(X_plot,i)
+        axs[0].plot(X_plot, Y_plot, color='r')
+        axs[0].plot(X_plot, Y_plot, label=r'$f(x)$',linewidth=2,color=col[i])
+
+    for i in range(2):
+        X_plot = torch.linspace(-3, 3, 1000)
+        Y_plot = outcome_funcs.uncfounded_func(X_plot,i)
+        axs[1].plot(X_plot, Y_plot, color='r')
+        axs[1].plot(X_plot, Y_plot, label=r'$f(x)$',linewidth=2,color=col[i])
+
+    X_plot = torch.linspace(-3, 3, 1000)
+    Y_plot = outcome_funcs.uncfounded_func(X_plot,1) - outcome_funcs.uncfounded_func(X_plot,0)
+    axs[2].plot(X_plot, Y_plot, color='r')
+    axs[2].plot(X_plot, Y_plot, label=r'$f(x)$',linewidth=2,color="red")
+
+    return fig,axs
